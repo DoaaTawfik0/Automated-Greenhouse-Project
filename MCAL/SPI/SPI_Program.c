a2a4c3c30ff1cee76f31c0b391fda441fb059b0f
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2024-07-06 13:39:20 +0300

    Update Pins Configuration

--- MCAL/SPI/SPI_Program.c
+++ MCAL/SPI/SPI_Program.c
@@ -11,8 +11,10 @@
 #include "../../LIB/BIT_MATH.h"
 #include "../../LIB/ERROR_STATE.h"
 
-#include "SPI_Interface.h"
+#include "../DIO/DIO_Interface.h"
+
 #include "SPI_Register.h"
+#include "SPI_Interface.h"
 #include "SPI_Private.h"
 #include "SPI_Config.h"
 
@@ -28,22 +30,39 @@
 /*****************************************************************************/
 /*****************************************************************************/
 
-ES_t SPI_MasterInit(SPI_stats spi_stat){
+ES_t SPI_MasterInit(SPI_stats spi_stat)
+{
 
- ES_t  Local_enuErrorState  = ES_NOK;
+	ES_t  Local_enuErrorState  = ES_NOK;
 
-    if(spi_stat==SPI_POLLING){
+	if(spi_stat==SPI_POLLING)
+	{
 
-    	 /* Set MOSI and SCK output, all others input */
-         //code
-    	 /* Enable SPI, Master, set clock rate fck/16 */
-         SPCR = (1<<6)|(1<<4)|(1<<0);
+		/* Set MOSI and SCK output, all others input */
+		DIO_enuSetPinDirection(MOSI_PORT , MOSI_PIN , OUTPUT);
+		DIO_enuSetPinDirection(SCK_PORT , SCK_PIN , OUTPUT);
 
-         Local_enuErrorState=ES_OK;
+		DIO_enuSetPinDirection(MISO_PORT , MISO_PIN , INPUT);
+		DIO_enuSetPinDirection(SS_PORT , SS_PIN , INPUT);
 
-    }
 
-    return Local_enuErrorState;
+		/* Enable SPI, Master, set clock rate fck/16 */
+		SPCR = (1<<6)|(1<<4)|(1<<0);
+
+		/* If SS is configured as an input and is
+		   driven low while MSTR is set, MSTR will
+           be cleared*/
+		if (!(GET_BIT(SPCR , 4)))
+		{
+			// Re-enable Master mode
+			SPCR |= (1<<4);
+		}
+
+		Local_enuErrorState=ES_OK;
+
+	}
+
+	return Local_enuErrorState;
 
 }
 
@@ -58,23 +77,31 @@ ES_t SPI_MasterInit(SPI_stats spi_stat){
 /*****************************************************************************/
 /*****************************************************************************/
 
-ES_t SPI_SlaveInit(SPI_stats spi_stat){
-	
+ES_t SPI_SlaveInit(SPI_stats spi_stat)
+{
+
 	ES_t  Local_enuErrorState  = ES_NOK;
 
-    if(spi_stat==SPI_POLLING){
+	if(spi_stat==SPI_POLLING)
+	{
 
-    	 /* Set MISO output, all others input */
-         //code
-         /* Enable SPI */
-         SPCR = (1<<6);
+		/* Set MISO output, all others input */
+		DIO_enuSetPinDirection(MISO_PORT , MISO_PIN , OUTPUT);
+
+		DIO_enuSetPinDirection(SCK_PORT , SCK_PIN , INPUT);
+		DIO_enuSetPinDirection(MISO_PORT , MISO_PIN , INPUT);
+		DIO_enuSetPinDirection(SS_PORT , SS_PIN , INPUT);
 
 
-         Local_enuErrorState=ES_OK;
+		/* Enable SPI */
+		SPCR = (1<<6);
 
-    }
 
-    return Local_enuErrorState;
+		Local_enuErrorState=ES_OK;
+
+	}
+
+	return Local_enuErrorState;
 
 }
 
@@ -89,20 +116,21 @@ ES_t SPI_SlaveInit(SPI_stats spi_stat){
 /*****************************************************************************/
 /*****************************************************************************/
 
-ES_t SPI_MasterTransmit(SPI_stats spi_stat,u8 Copy_u8Data){
+ES_t SPI_MasterTransmit(SPI_stats spi_stat,u8 Copy_u8Data)
+{
 	ES_t  Local_enuErrorState  = ES_NOK;
 
 	if(spi_stat==SPI_POLLING){
 
-	     /* Start transmission */
-        SPDR = Copy_u8Data;
-        /* Wait for transmission complete */
-        while(!(SPSR & (1<<7)));
-    }
+		/* Start transmission */
+		SPDR = Copy_u8Data;
+		/* Wait for transmission complete */
+		while(!(SPSR & (1<<7)));
+	}
 
 
-	 return Local_enuErrorState;
- 
+	return Local_enuErrorState;
+
 
 }
 
@@ -116,18 +144,19 @@ ES_t SPI_MasterTransmit(SPI_stats spi_stat,u8 Copy_u8Data){
 /*****************************************************************************/
 /*****************************************************************************/
 /*****************************************************************************/
-ES_t SPI_SlaveReceive(SPI_stats spi_stat,u8* Copy_pu8Data){
+ES_t SPI_SlaveReceive(SPI_stats spi_stat,u8* Copy_pu8Data)
+{
 	ES_t  Local_enuErrorState  = ES_NOK;
 
 	if(spi_stat==SPI_POLLING){
 
-	     /* Wait for reception complete */
-         while(!(SPSR & (1<<7)));
-         /* Return data register */
-         *Copy_pu8Data= SPDR;
-    }
+		/* Wait for reception complete */
+		while(!(SPSR & (1<<7)));
+		/* Return data register */
+		*Copy_pu8Data= SPDR;
+	}
 
 
-	 return Local_enuErrorState;
+	return Local_enuErrorState;
 
 }
