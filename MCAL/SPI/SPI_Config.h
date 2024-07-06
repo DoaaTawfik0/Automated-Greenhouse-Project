aff738e3a62e813b530fcece11a12eed01abe0fb
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2024-07-06 22:37:45 +0300

    SPI Driver

---
+++ MCAL/SPI/SPI_Config.h
@@ -0,0 +1,19 @@
+#ifndef   SPI_CONFIG_H_
+#define   SPI_CONFIG_H_
+
+
+#define   MOSI_PORT    DIO_PORTB
+#define   MOSI_PIN     DIO_PIN5
+
+#define   MISO_PORT    DIO_PORTB
+#define   MISO_PIN     DIO_PIN6
+
+#define   SCK_PORT     DIO_PORTB
+#define   SCK_PIN      DIO_PIN7
+
+#define   SS_PORT      DIO_PORTB
+#define   SS_PIN       DIO_PIN4
+
+#define   SPI_STATE    SPI_MASTER
+
+#endif
