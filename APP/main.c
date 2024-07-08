c4350a629bdde51b51c6830ef1fd3d2313d3f396
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2024-07-08 12:00:05 +0300

    App Files

--- APP/main.c
+++ APP/main.c
@@ -7,13 +7,17 @@
 #include "../MCAL/DIO/DIO_Interface.h"
 #include "../MCAL/Port/Port_Interface.h"
 #include "../MCAL/ADC/ADC_Interface.h"
+#include "../MCAL/SPI/SPI_Interface.h"
 
 
 #include "../HAL/Character_LCD/CLCD_Interface.h"
 #include "../HAL/DC_Motor/DC_Motor_Interface.h"
 #include "../HAL/LED/LED_Interface.h"
+#include "../HAL/LM35/LM35_Interface.h"
 
 #include "util/delay.h"
+#include "APP_Interface.h"
+
 
 
 int main()
@@ -21,6 +25,7 @@ int main()
 
 	while(1)
 	{
+
 
 	}
 
